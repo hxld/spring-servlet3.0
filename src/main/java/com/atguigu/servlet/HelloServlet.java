63c43c4bae879881600a587ff0477663e3c9c9f6
hxld <hxld88888@163.com>
2022-08-10 18:09:18 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/servlet/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.atguigu.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author hxld
+ * @create 2022-08-10 17:48
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        resp.getWriter().write("hello...");
+    }
+}
